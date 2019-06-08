572f0bb664d2b45a4ec4731692205a36e19d7dc9
yuwei <davidabm@163.com>
2019-06-08 15:36:50 +0800

    2

--- js/script.js
+++ js/script.js
@@ -19,14 +19,4 @@ function reqListener() {
 }
 
 
-window.onload = function() {
-  var oReq = new XMLHttpRequest();
-  oReq.addEventListener("load", reqListener);
-  oReq.open("GET", "config.json");
-  oReq.send();
 
-  let igElem = document.getElementById('instagram');
-  if (igElem.href === 'https://www.instagram.com/') {
-    document.querySelector('div.footer').remove();
-  }
-};
